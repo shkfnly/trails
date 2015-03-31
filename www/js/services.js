2a54eee22e0f70324f6aa3afab9f7b96c3bdfa46
Ashoka Finley <ashoka.finley@gmail.com>
2015-03-31 15:03:59 -0500

    Begin Trail Implementation

--- www/js/services.js
+++ www/js/services.js
@@ -4,25 +4,41 @@ angular.module('starter.services', [])
     id: 0,
     image: 'https://hikinginthesmokies.files.wordpress.com/2012/09/indian-creek-trail-august-07-2012.jpg',
     name: 'Trail #1',
-    desc: 'This is the first trail'
+    desc: 'This is the first trail',
+    points: [{
+      name: 'Tenderloin National Forest',
+      lat: 37.784430,
+      lon: -122.414966
+    }, {
+      name: 'Glide Graze the Roof',
+      lat: 37.785193,
+      lon: -122.411565
+    }, { 
+      name: 'Tenderloin Neighborhood Development Corporation',
+      lat: 37.784438,
+      lon: -122.409913
+    }
+]
   }, {
     id: 1,
     image: 'https://az-flagstaff3.civicplus.com/images/pages/N1521/Flagstaff_Urban_Trails_and_Bikeways_Map-Sample2-2011.jpg',
     name: 'Trail #2',
-    desc: 'This is the second trail'
+    desc: 'This is the second trail',
+    points: []
   }, {
     id: 2,
     image: 'http://www.travelandleisure.com/sites/default/files/styles/image_300x300/public/images/amexpub/0041/6531/201212-ss-urban-running-trails-central-park.jpg?itok=OML0PhBn',
     name: 'Trail #3',
-    desc: 'This is the third trail'
+    desc: 'This is the third trail',
+    points: []
   }];
 
   return {
     all: function() {
       return trails;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(trails) {
+      trails.splice(trails.indexOf(trail), 1);
     },
     get: function(trailId) {
       for (var i = 0; i < trails.length; i++) {
